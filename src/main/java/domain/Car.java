0d62367c5f1c31870f534390487d7b2cfeeeaf74
XabiAbaunz <Xabi@DESKTOP-PNFEN4V>
2024-04-03 10:15:36 +0200

    Car eta Movement klaseak

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Car {
+
+	private Driver driver;
+	private Ride ride;
+	
+	public Car() {
+		super();
+	}
+	
+	
+}
---
+++ src/main/java/domain/Movement.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.Date;
+
+public class Movement {
+	
+	private User user;
+	private double diruKop;
+	private Date data;
+	
+	public Movement(User user, double diruKop, Date data) {
+		super();
+		this.user = user;
+		this.diruKop = diruKop;
+		this.data = data;
+	}
+
+	public double getDiruKop() {
+		return diruKop;
+	}
+
+	public void setDiruKop(double diruKop) {
+		this.diruKop = diruKop;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "[diruKop=" + diruKop + ", data=" + data + "]";
+	}
+	
+	
+	
+	
+}
